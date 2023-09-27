b8c8814143683eb24633a4b59bb613a06ec69f80
Pradnya Sharad Kulkarni <89025442+Thepradnya01@users.noreply.github.com>
2023-09-27 09:49:54 +0530

    Update fibonacciSeries.java

--- fibonacciSeries.java
+++ fibonacciSeries.java
@@ -1,10 +1,24 @@
 package Basics;
+import java.util.*;
 
 public class fibonacciSeries {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter value of n:");
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		a[0]=1;
+		a[1]=1;
+		
+		for(int i=2;i<n;i++) {
+			a[i]=a[i-1]+a[i-2];
+		}
+		
+		for(int i=0;i<n;i++) {
+			System.out.println(a[i]);
+		}
 	}
 
 }
